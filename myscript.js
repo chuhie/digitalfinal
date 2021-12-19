14a5a0414c150664893494ecb36d1f961e947005
chuhie <62639865+chuhie@users.noreply.github.com>
2021-12-20 02:50:22 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,187 @@
+function whtieOn_1() {
+    
+    var dama = document.getElementById('sp1');
+    
+    
+    if (dama.src.match("d_03")) {
+        dama.src = "img/a-03.png";
+        document.body.style.background="#ffffff";
+        document.getElementById('sp2').style.visibility ='visible';
+        document.getElementById('sp3').style.visibility ='visible';
+        document.getElementById('sp4').style.visibility ='visible';
+        document.getElementById('sp5').style.visibility ='visible';
+  
+        document.getElementById('b_7').style.visibility ='visible';
+  
+        document.getElementById('b_1').style.visibility ='visible';
+        document.getElementById('b_2').style.visibility ='visible';
+        document.getElementById('b_3').style.visibility ='visible';
+        document.getElementById('b_4').style.visibility ='visible';
+        document.getElementById('b_5').style.visibility ='visible';
+        document.getElementById('b_6').style.visibility ='visible';
+    } else {
+        dama.src = "img/d_03.png";
+        document.body.style.background="#000000";
+        document.getElementById('sp2').style.visibility ='hidden';
+        document.getElementById('sp3').style.visibility ='hidden';
+        document.getElementById('sp4').style.visibility ='hidden';
+        document.getElementById('sp5').style.visibility ='hidden';
+        document.getElementById('b_1').style.visibility ='hidden';
+        document.getElementById('b_2').style.visibility ='hidden';
+        document.getElementById('b_3').style.visibility ='hidden';
+        document.getElementById('b_4').style.visibility ='hidden';
+        document.getElementById('b_5').style.visibility ='hidden';
+        document.getElementById('b_6').style.visibility ='hidden';
+        document.getElementById('b_7').style.visibility ='hidden';
+    }
+  }
+  
+  
+  
+  function whtieOn_2() {
+      
+    var dama = document.getElementById('sp2');
+    
+    if (dama.src.match("d_04")) {
+        dama.src = "img/a-04.png";
+        document.body.style.background="#ffffff";
+        document.getElementById('sp1').style.visibility ='visible';
+        document.getElementById('sp3').style.visibility ='visible';
+        document.getElementById('sp4').style.visibility ='visible';
+        document.getElementById('sp5').style.visibility ='visible';
+        document.getElementById('b_1').style.visibility ='visible';
+        document.getElementById('b_2').style.visibility ='visible';
+        document.getElementById('b_3').style.visibility ='visible';
+        document.getElementById('b_4').style.visibility ='visible';
+        document.getElementById('b_5').style.visibility ='visible';
+        document.getElementById('b_6').style.visibility ='visible';
+        document.getElementById('b_7').style.visibility ='visible';
+    } else {
+        dama.src = "img/d_04.png";
+        document.body.style.background="#000000";
+        document.getElementById('sp1').style.visibility ='hidden';
+        document.getElementById('sp3').style.visibility ='hidden';
+        document.getElementById('sp4').style.visibility ='hidden';
+        document.getElementById('sp5').style.visibility ='hidden';
+        document.getElementById('b_1').style.visibility ='hidden';
+        document.getElementById('b_2').style.visibility ='hidden';
+        document.getElementById('b_3').style.visibility ='hidden';
+        document.getElementById('b_4').style.visibility ='hidden';
+        document.getElementById('b_5').style.visibility ='hidden';
+        document.getElementById('b_6').style.visibility ='hidden';
+        document.getElementById('b_7').style.visibility ='hidden';
+  
+    }
+  }
+  
+  
+  
+  function whtieOn_3() {
+      
+    var dama = document.getElementById('sp3');
+    
+    if (dama.src.match("d_05")) {
+        dama.src = "img/a-05.png";
+        document.body.style.background="#ffffff";
+        document.getElementById('sp1').style.visibility ='visible';
+        document.getElementById('sp2').style.visibility ='visible';
+        document.getElementById('sp4').style.visibility ='visible';
+        document.getElementById('sp5').style.visibility ='visible';
+        document.getElementById('b_1').style.visibility ='visible';
+        document.getElementById('b_2').style.visibility ='visible';
+        document.getElementById('b_3').style.visibility ='visible';
+        document.getElementById('b_4').style.visibility ='visible';
+        document.getElementById('b_5').style.visibility ='visible';
+        document.getElementById('b_6').style.visibility ='visible';
+        document.getElementById('b_7').style.visibility ='visible';
+    } else {
+        dama.src = "img/d_05.png";
+        document.body.style.background="#000000";
+        document.getElementById('sp1').style.visibility ='hidden';
+        document.getElementById('sp2').style.visibility ='hidden';
+        document.getElementById('sp4').style.visibility ='hidden';
+        document.getElementById('sp5').style.visibility ='hidden';
+        document.getElementById('b_1').style.visibility ='hidden';
+        document.getElementById('b_2').style.visibility ='hidden';
+        document.getElementById('b_3').style.visibility ='hidden';
+        document.getElementById('b_4').style.visibility ='hidden';
+        document.getElementById('b_5').style.visibility ='hidden';
+        document.getElementById('b_6').style.visibility ='hidden';
+        document.getElementById('b_7').style.visibility ='hidden';
+    }
+  }
+  
+  
+  
+  function whtieOn_4() {
+      
+    var dama = document.getElementById('sp4');
+    
+    if (dama.src.match("d_06")) {
+        dama.src = "img/a-06.png";
+        document.body.style.background="#ffffff";
+        document.getElementById('sp1').style.visibility ='visible';
+        document.getElementById('sp2').style.visibility ='visible';
+        document.getElementById('sp3').style.visibility ='visible';
+        document.getElementById('sp5').style.visibility ='visible';
+        document.getElementById('b_1').style.visibility ='visible';
+        document.getElementById('b_2').style.visibility ='visible';
+        document.getElementById('b_3').style.visibility ='visible';
+        document.getElementById('b_4').style.visibility ='visible';
+        document.getElementById('b_5').style.visibility ='visible';
+        document.getElementById('b_6').style.visibility ='visible';
+        document.getElementById('b_7').style.visibility ='visible';
+    } else {
+        dama.src = "img/d_06.png";
+        document.body.style.background="#000000";
+        document.getElementById('sp1').style.visibility ='hidden';
+        document.getElementById('sp2').style.visibility ='hidden';
+        document.getElementById('sp3').style.visibility ='hidden';
+        document.getElementById('sp5').style.visibility ='hidden';
+        document.getElementById('b_1').style.visibility ='hidden';
+        document.getElementById('b_2').style.visibility ='hidden';
+        document.getElementById('b_3').style.visibility ='hidden';
+        document.getElementById('b_4').style.visibility ='hidden';
+        document.getElementById('b_5').style.visibility ='hidden';
+        document.getElementById('b_6').style.visibility ='hidden';
+        document.getElementById('b_7').style.visibility ='hidden';
+    }
+  }
+  
+  
+  function whtieOn_5() {
+      
+    var dama = document.getElementById('sp5');
+    
+    if (dama.src.match("d_07")) {
+        dama.src = "img/a-07.png";
+        document.body.style.background="#ffffff";
+        document.getElementById('sp1').style.visibility ='visible';
+        document.getElementById('sp2').style.visibility ='visible';
+        document.getElementById('sp3').style.visibility ='visible';
+        document.getElementById('sp4').style.visibility ='visible';
+        document.getElementById('b_1').style.visibility ='visible';
+        document.getElementById('b_2').style.visibility ='visible';
+        document.getElementById('b_3').style.visibility ='visible';
+        document.getElementById('b_4').style.visibility ='visible';
+        document.getElementById('b_5').style.visibility ='visible';
+        document.getElementById('b_6').style.visibility ='visible';
+        document.getElementById('b_7').style.visibility ='visible';
+    } else {
+        dama.src = "img/d_07.png";
+        document.body.style.background="#000000";
+        document.getElementById('sp1').style.visibility ='hidden';
+        document.getElementById('sp2').style.visibility ='hidden';
+        document.getElementById('sp3').style.visibility ='hidden';
+        document.getElementById('sp4').style.visibility ='hidden';
+        document.getElementById('b_1').style.visibility ='hidden';
+        document.getElementById('b_2').style.visibility ='hidden';
+        document.getElementById('b_3').style.visibility ='hidden';
+        document.getElementById('b_4').style.visibility ='hidden';
+        document.getElementById('b_5').style.visibility ='hidden';
+        document.getElementById('b_6').style.visibility ='hidden';
+        document.getElementById('b_7').style.visibility ='hidden';
+    }
+  }
+  
+
